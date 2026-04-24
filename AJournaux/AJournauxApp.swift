5dc9749ae6c25fb6ce92909054d23e9f15c5d330
Amen <a.ftermin2@gmail.com>
2026-04-24 23:08:16 +0700

    Initial Commit

---
+++ AJournaux/AJournauxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AJournauxApp.swift
+//  AJournaux
+//
+//  Created by Amén on 24-04-2026.
+//
+
+import SwiftUI
+
+@main
+struct AJournauxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
