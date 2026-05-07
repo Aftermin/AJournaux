9a21e7b318eee621ad85603ff43291124c161df7
Amen <a.ftermin2@gmail.com>
2026-05-08 00:33:03 +0700

    Redesign and add old paper template for moment detail

---
+++ AJournaux/Views/Home/JournalPrompts.swift
@@ -0,0 +1,8 @@
+//
+//  JournalPrompts.swift
+//  AJournaux
+//
+//  Created by Amén on 26-04-2026.
+//
+
+import Foundation
---
+++ AJournaux/Views/Moment Detail/EntryDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  EntryDetailView.swift
+//  AJournaux
+//
+//  Created by Amén on 26-04-2026.
+//
+
+import Foundation
