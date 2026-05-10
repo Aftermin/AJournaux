181b5bc6a4845f9a26184af433ca4f4d394319da
Amen <a.ftermin2@gmail.com>
2026-05-11 00:10:15 +0700

    Add infomation about the user ex.name dateofbirth profile Add 6 Pics arndom option

---
+++ AJournaux/Models/UserProfile.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfile.swift
+//  AJournaux
+//
+//  Created by Amén on 10-05-2026.
+//
+
+import Foundation
---
+++ AJournaux/Views/Home/ProfileSheetView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSheetView.swift
+//  AJournaux
+//
+//  Created by Amén on 10-05-2026.
+//
+
+import Foundation
